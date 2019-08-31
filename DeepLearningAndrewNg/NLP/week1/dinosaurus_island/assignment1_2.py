5377aa19f9dc553024e64c6257c27ef1873ab5b4
ownia <451686892@qq.com>
2019-08-31 13:58:52 +0800

    add NLP assignment1_3

--- DeepLearningAndrewNg/NLP/week1/dinosaurus_island/assignment1_2.py
+++ DeepLearningAndrewNg/NLP/week1/dinosaurus_island/assignment1_2.py
@@ -227,6 +227,4 @@ if __name__ == '__main__':
     # Training the model
     parameters = model(data, ix_to_char, char_to_ix)
     # Writing like Shakespeare
-    print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
-    model.fit(x, y, batch_size=128, epochs=1, callbacks=[print_callback])
     generate_output()
