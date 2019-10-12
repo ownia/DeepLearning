beea7fbdbafb09f96d270785dd1450f17780d4c1
ownia <451686892@qq.com>
2019-10-12 21:43:18 +0800

    add NLP week2 assignment2_3

---
+++ DeepLearningAndrewNg/NLP/week2/Emojify/assignment2_3.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+np.random.seed(0)
+from keras.models import Model
+from keras.layers import Dense, Input, Dropout, LSTM, Activation
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+from keras.initializers import glorot_uniform
+
+np.random.seed(1)
